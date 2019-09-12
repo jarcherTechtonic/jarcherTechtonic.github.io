56a24d1daf0dc3926c1d2113d515dbc435c1cbc8
Joshua "Moose" Archer <joshua.archer@techtonic.com>
2019-09-12 15:49:43 -0600

    2d raycasting

---
+++ 2d_raycasting/particle.js
@@ -0,0 +1,41 @@
+class Particle {
+  constructor() {
+    this.pos = createVector(width / 2, height / 2);
+    this.rays = [];
+    for (let a = 0; a < 360; a += 5) {
+      this.rays.push(new Ray(this.pos, radians(a)));
+    }
+  }
+
+  update(x, y) {
+    this.pos.set(x, y);
+  }
+
+  look(walls) {
+    for (let ray of this.rays) {
+      let closest = null;
+      let record = Infinity;
+      for (let wall of walls) {
+        const point = ray.cast(wall);
+        if (point) {
+          let distance = p5.Vector.dist(this.pos, point)
+          if (distance < record) {
+            record = distance;
+            closest = point;
+          }
+        }
+      }
+      if (closest) {
+        line(this.pos.x, this.pos.y, closest.x, closest.y);
+      }
+    }
+  }
+
+  show() {
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, 4);
+    for (let ray of this.rays) {
+      ray.show();
+    }
+  }
+}
